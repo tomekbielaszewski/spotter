a9ccf55684a5dc486798845713fcb8a68b84ff94
tbielaszewski <tomekbielaszewski@gmail.com>
2020-03-03 11:57:10 +0100

    ImageLocator docs

--- src/main/java/pl/grizwold/screenautomation/ImageLocator.java
+++ src/main/java/pl/grizwold/screenautomation/ImageLocator.java
@@ -16,6 +16,21 @@ import java.util.List;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * {@link ImageLocator} class is responsible for locating given {@link Icon} on bigger "base" image provided in constructor.<br/>
+ * Pure magenta pixels on subimage are ignored. rgb(255,0,255)<br/>
+ * Finding subimages which pixel colors are much less common on base image is much faster. Especially when most unique
+ * pixel color is located on upper left corner of subimage.<br/><br/>
+ *
+ * Algorithm for locating {@link Icon} subimage goes as follows:<br/>
+ * - Preprocess base image by mapping each pixels color to all locations on screen. Map<PixelColor, LocationOnScreen><br/>
+ * - locate first non-magenta pixel on subimage starting from upper left corner and going down<br/>
+ * - find all locations of found pixel color on base image<br/>
+ * - for every next non-magenta pixel:<br/>
+ *   - calculate vector between location of first non-magenta pixel (in subimage coordinates) and this pixel (in subimage coordinates)<br/>
+ *   - find all locations of this pixel color on base image<br/>
+ *   - filter out all first pixel locations which are not equal to next pixel location subtracted by vector calculated above<br/>
+ **/
 @Slf4j
 public class ImageLocator {
     private static final int MASK = -65281; //pure magenta color
@@ -27,7 +42,10 @@ public class ImageLocator {
     private String debug_saveStepsDirectory;
     private int amountOfLastFoundPixels = -1;
 
-    public ImageLocator(BufferedImage base) {
+    /**
+     * @param base image on which Icon will be searched
+     */
+    public ImageLocator(@Nonnull BufferedImage base) {
         this.base = base;
         buildColorMap();
         setupDebugFlags();
@@ -47,10 +65,15 @@ public class ImageLocator {
         log.debug("Building screen color map took: {} ms", (System.currentTimeMillis() - start));
     }
 
+    /**
+     * Founds all locations of subimage on base image.
+     * @param subimage {@link BufferedImage} wrapper of searched image
+     * @return all found locations of given {@link Icon} subimage found on base image
+     */
     @Nonnull
-    public List<Point> locate(@Nonnull final Icon icon_) {
+    public List<Point> locate(@Nonnull final Icon subimage) {
         long start = System.currentTimeMillis();
-        final BufferedImage sample = icon_.getImage();
+        final BufferedImage sample = subimage.getImage();
         List<Point> possibleFirstPixels = new ArrayList<>();
         boolean startedSampling = false;
         Point[] firstSampledLocation = new Point[1];
@@ -73,16 +96,16 @@ public class ImageLocator {
                             .collect(Collectors.toList());
                 }
 
-                debugImage(this.base, icon_, possibleFirstPixels, x + "_" + y, start);
+                debugImage(this.base, subimage, possibleFirstPixels, x + "_" + y, start);
 
                 if (possibleFirstPixels.isEmpty()) {
-                    log.debug("Icon {} not found", icon_.getFilename());
+                    log.debug("Icon {} not found", subimage.getFilename());
                     return possibleFirstPixels;
                 }
             }
         }
 
-        log.debug("Locating icon \"{}\" took: {} ms", icon_.getFilename(), (System.currentTimeMillis() - start));
+        log.debug("Locating icon \"{}\" took: {} ms", subimage.getFilename(), (System.currentTimeMillis() - start));
 
         possibleFirstPixels = possibleFirstPixels.stream()
                 .map(p -> p.minus(firstSampledLocation[0]))
@@ -91,11 +114,22 @@ public class ImageLocator {
         return possibleFirstPixels;
     }
 
+    /**
+     * Activating image debugging will visualize each iteration of image locating algorithm by saving images with
+     * located possible first pixels in given directory. Possible first pixels are marked by magenta color and every next
+     * algorithm iteration should filter out some of the pixels (see algorithm description on {@link ImageLocator}).
+     * Last saved iteration will be clear base image in case of not finding the subimage, or single magenta pixel in case of
+     * finding single location or multiple pixels in case of finding many subimages on base image.
+     * @param directory where algorithm iteration steps will be visualized by saving image files
+     */
     public void activateDebugging(String directory) {
         this.debug_saveSteps = true;
         this.debug_saveStepsDirectory = Optional.ofNullable(directory).orElse("debug");
     }
 
+    /**
+     * Deactivates algorithm visualizer. See: activateDebugging
+     */
     public void deactivateDebugging() {
         this.debug_saveSteps = false;
         this.debug_saveStepsDirectory = null;
